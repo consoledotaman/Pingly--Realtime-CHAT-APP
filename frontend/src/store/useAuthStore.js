3c657a8f10da1b8ef38e15df0c9b53b18a943ca1
amanraj <raj.aman4001@gmail.com>
2025-07-11 15:29:13 +0530

    feat: create Profile page with updateProfile function
    Signed-off-by: amanraj <raj.aman4001@gmail.com>

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -62,9 +62,22 @@ export const useAuthStore = create((set) => ({
             toast.success("Logged out succesfully");
 
         }catch(error){
-            toast.error(error.response.data.message);
+            toast.error(error?.response?.data?.message || "Something went wrong");
         }
 
     },
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("error in update profile:", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
 
 }));
